6782feb16864821744dc15a60fddd084a35a69d8
Jens Ravn Jensen <jr@jensravn.com>
2018-01-13 09:31:21 +0100

    rotating rubiks

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import {
     Color,
     CubeGeometry,
+    Group,
     Mesh,
     MeshBasicMaterial,
     PerspectiveCamera,
@@ -13,34 +14,37 @@ const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight,
 const renderer = new WebGLRenderer();
 
 scene.background = new Color(0xffffff);
+camera.position.z = 5;
 renderer.setSize(window.innerWidth, window.innerHeight);
+
 document.body.appendChild(renderer.domElement);
 
 const geometry = new CubeGeometry(0.8, 0.8, 0.8);
 const material = new MeshBasicMaterial({ color: 0xff0000, wireframe: true });
 
-const offset = [-1, 0, 1];
+const offset = [0, 1, 2];
 
-const cube: any = {};
+const rubiks = new Group();
+const cube: Mesh[][][] = [[[], [], []], [[], [], []], [[], [], []]];
 for (const x of offset) {
     for (const y of offset) {
         for (const z of offset) {
-            const key = `cube${x + 1}${y + 1}${z + 1}`;
-            cube[key] = { position: { x, y, z }, mesh: new Mesh(geometry, material) };
-            cube[key].mesh.position.x = x;
-            cube[key].mesh.position.y = y;
-            cube[key].mesh.position.z = z;
-            if (!(x === 0 && y === 0 && z === 0)) {
-                scene.add(cube[key].mesh);
+            if (x !== 1 || y !== 1 || z !== 1) {
+                cube[x][y][z] = new Mesh(geometry, material);
+                cube[x][y][z].position.set(x - 1, y - 1, z - 1);
+                rubiks.add(cube[x][y][z]);
             }
         }
     }
 }
-
-camera.position.z = 5;
+scene.add(rubiks);
 
 const animate = () => {
     requestAnimationFrame(animate);
+
+    rubiks.rotation.x += 0.006;
+    rubiks.rotation.y += 0.008;
+
     renderer.render(scene, camera);
 };
 
